ed9226a155fabd70c06cde7b05d248d24df4bf20
oi <43892086+kkeigenkai@users.noreply.github.com>
2023-02-26 02:43:58 +0300

    Задача 3. Коллекционер Диего

---
+++ Training_3/collectorDiego/diego_test.go
@@ -0,0 +1,36 @@
+package collectordiego
+
+import "testing"
+
+type diegoTest struct {
+	diegoCnt int
+	diegoS   []int
+	collCnt  int
+	collS    []int
+	exp      []int
+}
+
+var tests = []diegoTest{
+	{1, []int{5}, 2, []int{4, 6}, []int{0, 1}},
+	{3, []int{100, 1, 50}, 3, []int{300, 0, 75}, []int{3, 0, 2}},
+	{0, []int{}, 0, []int{}, []int{0}},
+}
+
+func TestDiego(t *testing.T) {
+	for _, test := range tests {
+		got := Diego(test.diegoCnt, test.diegoS, test.collCnt, test.collS)
+		if len(got) != len(test.exp) {
+			t.Errorf("( %d, %v, %d, %v ) = %v,\n\twant: %v\n",
+				test.diegoCnt, test.diegoS, test.collCnt, test.collS, got, test.exp)
+			continue
+		}
+
+		for i, g := range got {
+			if test.exp[i] != g {
+				t.Errorf("( %d, %v, %d, %v ) = %v,\n\twant: %v\n",
+					test.diegoCnt, test.diegoS, test.collCnt, test.collS, got, test.exp)
+				break
+			}
+		}
+	}
+}
