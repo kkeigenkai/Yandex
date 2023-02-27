82d4e5f3b0964e80a93e7b33c65ca9e0ca260e58
oi <43892086+kkeigenkai@users.noreply.github.com>
2023-02-27 13:11:00 +0300

    Задача 20. Пирамидальная сортировка

---
+++ Training_3/pyramidSort/pyramidSort.go
@@ -0,0 +1,32 @@
+package pyramidsort
+
+func PyramidSort(cnt int, si []int) []int {
+	for i := len(si)/2 - 1; i >= 0; i-- {
+		si = heapSort(si, i, len(si))
+	}
+
+	for i := len(si) - 1; i >= 1; i-- {
+		si[0], si[i] = si[i], si[0]
+		si = heapSort(si, 0, i)
+	}
+	return si
+}
+
+func heapSort(si []int, i int, l int) []int {
+	max := 0
+	for i*2+1 < l {
+		if i*2+1 == l-1 || si[i*2+1] > si[i*2+2] {
+			max = i*2 + 1
+		} else {
+			max = i*2 + 2
+		}
+
+		if si[i] < si[max] {
+			si[i], si[max] = si[max], si[i]
+			i = max
+		} else {
+			break
+		}
+	}
+	return si
+}
---
+++ Training_3/pyramidSort/pyramidSort_test.go
@@ -0,0 +1,35 @@
+package pyramidsort
+
+import "testing"
+
+type pyramidSortTest struct {
+	cnt     int
+	si, exp []int
+}
+
+var tests = []pyramidSortTest{
+	{1, []int{1}, []int{1}},
+	{2, []int{3, 1}, []int{1, 3}},
+	{5, []int{3, 8, 1, 2, 9}, []int{1, 2, 3, 8, 9}},
+	{
+		5,
+		[]int{970230416, 819698993, 12233245, 983424780, 762955113, 552371399, 728993264, 46396314, 339526042},
+		[]int{12233245, 46396314, 339526042, 552371399, 728993264, 762955113, 819698993, 970230416, 983424780},
+	},
+}
+
+func TestPyramidSort(t *testing.T) {
+	for _, test := range tests {
+		got := PyramidSort(test.cnt, test.si)
+		if len(got) != len(test.exp) {
+			t.Errorf("( %d, %v ) = %v,\n\twant: %v\n", test.cnt, test.si, got, test.exp)
+			continue
+		}
+		for i := range got {
+			if got[i] != test.exp[i] {
+				t.Errorf("( %d, %v ) = %v,\n\twant: %v\n", test.cnt, test.si, got, test.exp)
+				break
+			}
+		}
+	}
+}
