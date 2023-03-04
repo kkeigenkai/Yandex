fdea03587c6028b7f592b4b66412cb6bfaf6d8be
Sergei Pshonnov <43892086+kkeigenkai@users.noreply.github.com>
2023-03-04 23:40:07 +0300

    Задача 19. Хипуй

--- Training_3/heap/heap.go
+++ Training_3/heap/heap.go
@@ -1,5 +1,17 @@
 package heap
 
-func Heap(ss []string) []int {
+import "github.com/kkeigenkai/yandex/util"
 
+func Heap(ss [][2]int) []int {
+	h := util.CreateSliceHeap()
+	res := make([]int, 0, len(ss))
+	for _, cmd := range ss {
+		switch cmd[0] {
+		case 0:
+			h.Push(cmd[1])
+		case 1:
+			res = append(res, h.Pop())
+		}
+	}
+	return res
 }
